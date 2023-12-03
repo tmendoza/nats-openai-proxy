be096a7bf2cb1ad88a9140776004d8d7581fe30a
Tony Mendoza <clueelf@tonymendoza.us>
2023-12-03 21:07:51 +0000

    Modified code to use new Openai API

--- src/nats-openai-proxy.py
+++ src/nats-openai-proxy.py
@@ -6,7 +6,6 @@ import argparse
 
 import nats
 from nats.aio.client import Client as NATS
-from nats.aio.nkeys import NKeys
 import ssl
 
 # Rest of the application code...
@@ -16,6 +15,7 @@ class OpenAINATSProxy:
         self.config = config
         self.nc = NATS()
 
+    """
     # Setup NATS connection with nkeys and TLS
     async def secure_connect_to_nats(self):
         options = {
@@ -49,33 +49,64 @@ class OpenAINATSProxy:
         except Exception as e:
             logging.error(f"Failed to connect to NATS server: {e}")
             exit(1)
+    """
 
     async def connect_to_nats(self):
+        options = {
+            "servers": config["nats"]["server"],
+            "name": "OpenAINATSProxy"
+        }
+
+        # Configure nkeys if provided
+        if "nkey_seed_file" in config["nats"]:
+            with open(config["nats"]["nkey_seed_file"]) as f:
+                seed = f.read()
+            nk = NKeys.from_seed(seed.encode())
+            options["nkeys_seed"] = seed
+            options["signature_cb"] = lambda nonce: nk.sign(nonce.encode())
+
         try:
-            await self.nc.connect(servers=[self.config['nats_server']])
+            await self.nc.connect(**options)
             logging.info("Connected to NATS Server")
         except Exception as e:
             logging.error(f"Failed to connect to NATS server: {e}")
             exit(1)
 
     async def listen_for_requests(self):
-        subject = self.config.get('subject', 'chat_requests')
+        # subject = self.config.get('subject', 'chat_requests')
+        subject = self.config['nats']['subject']
         await self.nc.subscribe(subject, cb=self.handle_request)
         logging.info(f"Listening for requests on '{subject}'")
 
     async def handle_request(self, msg):
         data = msg.data.decode()
+        logging.info(f"Handling request '{data}'")
         try:
+            logging.info("Making call to OpenAI...")
             response = self.query_openai(data)
+            logging.info(f"Publishing results back to client... {response}")
             await self.nc.publish(msg.reply, response.encode())
         except Exception as e:
             logging.error(f"Error handling request: {e}")
             await self.nc.publish(msg.reply, b"Error processing request")
 
     def query_openai(self, prompt):
-        openai.api_key = self.config['openai_api_key']
-        response = openai.Completion.create(engine=self.config.get('engine', "text-davinci-004"), prompt=prompt)
-        return response.choices[0].text.strip()
+        # optional; defaults to `os.environ['OPENAI_API_KEY']`
+        openai.api_key = self.config['nats']['openai_api_key']
+        logging.debug(f"Using OpenAI Key: '{openai.api_key}'")
+
+        completion = openai.chat.completions.create(
+            model= self.config['nats']['engine'],
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                },
+            ],
+        )
+
+        logging.debug(f"Returned completion: '{completion.choices[0].message.content}'")
+        return completion.choices[0].message.content
 
     async def run(self):
         await self.connect_to_nats()
@@ -88,11 +119,12 @@ def load_config(file_path):
 def parse_arguments():
     parser = argparse.ArgumentParser(description="OpenAI-NATS Proxy Service")
     parser.add_argument("--config", help="Path to the configuration file", default="config.yaml")
-    parser.add_argument("--nats_server", help="NATS server address")
+    parser.add_argument("--nats_server", help="NATS server address", default="nats://localhost:4222")
     parser.add_argument("--openai_api_key", help="OpenAI API key")
     parser.add_argument("--subject", help="NATS subject to listen on")
     parser.add_argument("--engine", help="OpenAI engine to use")
     parser.add_argument("--nkey_seed_file", help="Path to the NATS nkey seed file for authentication.")
+    parser.add_argument("--tls_enabled", help="NATS TLS Security enabled.", default=False)
     parser.add_argument("--tls_cert", help="Path to the TLS certificate file.")
     parser.add_argument("--tls_key", help="Path to the TLS key file.")
     parser.add_argument("--tls_ca", help="Path to the TLS CA file (optional).")
@@ -105,42 +137,34 @@ if __name__ == "__main__":
     # Load configuration file
     config = load_config(args.config)
 
-    """
-    nats:
-    server: "nats://localhost:4222"
-    subject: "openai_requests"
-    openai_api_key: "your-openai-api-key"
-    engine: "text-davinci-004"
-    nkey_seed_file: "path/to/nkey_seed_file"
-    tls:
-        enabled: true
-        cert: "path/to/cert.pem"
-        key: "path/to/key.pem"
-        ca: "path/to/ca.pem" # Optional
-    """
+
     # Override config with CLI arguments if provided
-    if args.nats.server:
-        config['nats']['server'] = args.nats.server
-    if args.nats.openai_api_key:
-        config['nats']['openai_api_key'] = args.nats.openai_api_key
-    if args.nats.subject:
-        config['nats']['subject'] = args.nats.subject
-    if args.nats.engine:
-        config['nats']['engine'] = args.nats.engine
-    if args.nats.nkey_seed_file:
-        config['nats']['nkey_seed_file'] = args.nats.nkey_seed_file
-    if args.nats.tls.enabled:
-        config['nats']['tls']['enabled'] = args.nats.tls.enabled
-    if args.nats.tls.cert:
-        config['nats']['tls']['cert'] = args.nats.tls.cert
-    if args.nats.tls.key:
-        config['nats']['tls']['key'] = args.nats.tls.key
-    if args.nats.tls.ca:
-        config['nats']['tls']['ca'] = args.nats.tls.ca
+    if args.nats_server:
+        config['nats']['server'] = args.nats_server
+    if args.openai_api_key:
+        config['nats']['openai_api_key'] = args.openai_api_key
+    if args.subject:
+        config['nats']['subject'] = args.subject
+    if args.engine:
+        config['nats']['engine'] = args.engine
+    if args.nkey_seed_file:
+        config['nats']['nkey_seed_file'] = args.nkey_seed_file
+    if args.tls_enabled:
+        config['nats']['tls']['enabled'] = args.tls_enabled
+    if args.tls_cert:
+        config['nats']['tls']['cert'] = args.tls_cert
+    if args.tls_key:
+        config['nats']['tls']['key'] = args.tls_key
+    if args.tls_ca:
+        config['nats']['tls']['ca'] = args.tls_ca
+
 
     # Setup logging
     logging.basicConfig(level=logging.INFO)
 
     # Run the service
-    proxy = OpenAINATSProxy(config)
-    asyncio.run(proxy.run())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(OpenAINATSProxy(config).run())
+    loop.run_forever()
+    loop.close()
+
